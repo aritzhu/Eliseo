34404e5bf92916bb76ef2586affe563358eeb1d8
Etxabe <150168621+Etxabe@users.noreply.github.com>
2023-11-08 12:17:33 +0100

    Add files via upload

---
+++ siu.c
@@ -0,0 +1,14 @@
+int fallo = 0;
+for(int i = 0;i<150;i++){
+    if(a[i]==','){
+        if(a[i+1]==','){
+            fallo++;
+        }
+    }
+}
+if(fallo < 1){
+    //leer dato
+}
+else{
+    continue
+}
